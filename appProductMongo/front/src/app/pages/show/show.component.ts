52191c995495148eebedae9d73fece73c2cd7b5a
Pablo Kohls <kohlsalejandro1999@gmail.com>
2023-06-28 23:15:43 -0500

    creando vista de show o ver los productos

--- appProductMongo/front/src/app/pages/show/show.component.ts
+++ appProductMongo/front/src/app/pages/show/show.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faCirclePlus, faPen, faTrash} from '@fortawesome/free-solid-svg-icons'
+import { Product } from 'src/app/models/product.interface';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
-export class ShowComponent {
+export class ShowComponent implements OnInit{
 
+  faCirclePlus = faCirclePlus
+  faPen = faPen
+  faTrash = faTrash
+
+  products: Product[]=[]
+
+  constructor(private crudService: CrudService){
+
+  }
+
+  ngOnInit(): void {
+      this.crudService.getProducts().subscribe((res:Product[])=>{
+        // console.log(res);
+        this.products = res;
+      })
+  }
+  delete(id:any,index:any){
+    console.log(id);
+  }
 }
